986a5c8342ad56e60337b74e7248b10659c0f2ff
Ru\o <racaballeroa@hotmail.com>
2019-12-11 14:07:09 -0600

    Se da estilos

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,51 @@
 import React from 'react';
+import './styles/login-v1.css';
 
 function Login() {
   return (
-    <div class="kt-login-v1__wrapper">
-  				<div class="kt-login-v1__container">
-  					<h3 class="kt-login-v1__title">
+    <div className="kt-login-v1__body">
+			{/*<!--begin::Section-->*/}
+			<div className="kt-login-v1__section">
+				<div className="kt-login-v1__info">
+					<h3 className="kt-login-v1__intro">Whatever CTA's wave purpose important exit element</h3>
+					<p>Lorem ipsum lian amet estudiat</p>
+				</div>
+			</div>
+			{/*<!--begin::Section-->*/}
+
+			{/*<!--begin::Separator-->*/}
+			<div className="kt-login-v1__seaprator"></div>
+			{/*<!--end::Separator-->*/}
+    <div className="kt-login-v1__wrapper">
+  				<div className="kt-login-v1__container">
+  					<h3 className="kt-login-v1__title">
   						Sign To Account
   					</h3>
   					{/*<!--begin::Form-->*/}
-  					<form class="kt-login-v1__form kt-form" action="" autocomplete="off">
-  						<div class="form-group">
-  							<input class="form-control" type="text" placeholder="Username"
-                 name="username" autocomplete="off">
+  					<form className="kt-login-v1__form kt-form" action="" autoComplete="off">
+  						<div className="form-group">
+  							<input className="form-control" type="text" placeholder="Username"
+                 name="username" autoComplete="off">
                 </input>
   						</div>
-  						<div class="form-group">
-  							<input class="form-control" type="password"
-                placeholder="Password" name="password" autocomplete="off">
+  						<div className="form-group">
+  							<input className="form-control" type="password"
+                placeholder="Password" name="password" autoComplete="off">
                 </input>
   						</div>
 
-  						<div class="kt-login-v1__actions">
-  							<a href="#" class="kt-login-v1__forgot">
+  						<div className="kt-login-v1__actions">
+  							<a href="#" className="kt-login-v1__forgot">
   								Forgot Password ?
   							</a>
-  							<button type="submit" class="btn btn-pill btn-elevate">Sign In</button>
+  							<button type="submit" className="btn btn-pill btn-elevate">Sign In</button>
   						</div>
   					</form>
   					{/*<!--end::Form-->*/}
 
   					{/*<!--begin::Divider-->*/}
-  					<div class="kt-login-v1__divider">
-  						<div class="kt-divider">
+  					<div className="kt-login-v1__divider">
+  						<div className="kt-divider">
   							<span></span>
   							<span>OR</span>
   							<span></span>
@@ -40,24 +54,25 @@ function Login() {
           {/*<!--end::Divider--*/}
 
   					{/*<!--begin::Options-->*/}
-  					<div class="kt-login-v1__options">
-  						<a href="#" class="btn">
-  							<i class="fab fa-facebook-f"></i>
+  					<div className="kt-login-v1__options">
+  						<a href="#" className="btn">
+  							<i className="fab fa-facebook-f"></i>
   							Fcebook
   						</a>
-  						<a href="#" class="btn">
-  							<i class="fab fa-twitter"></i>
+  						<a href="#" className="btn">
+  							<i className="fab fa-twitter"></i>
   							Twitter
   						</a>
-  						<a href="#" class="btn">
-  							<i class="fab fa-google"></i>
+  						<a href="#" className="btn">
+  							<i className="fab fa-google"></i>
   							Google
   						</a>
   					</div>
   					{/*<!--end::Options-->*/}
   				</div>
   			</div>
- )
+      </div>
+ );
 }
 
-export default Login
+export default Login;
